2d2c99df410a77791db1dd5ece8958edf916ddb7
NMZ0429 <gen0429@icloud.com>
2021-10-06 00:45:05 +0900

    add code

---
+++ data.py
@@ -0,0 +1,80 @@
+import torch
+import torchvision.transforms as T
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader, Dataset  # type: ignore
+from torchvision.io import read_image
+
+
+class PetfinderDataset(Dataset):
+    def __init__(self, df, image_size=224):
+        self._X = df["Id"].values
+        self._y = None
+        if "Pawpularity" in df.keys():
+            self._y = df["Pawpularity"].values
+        self._transform = T.Resize([image_size, image_size])
+
+    def __len__(self):
+        return len(self._X)
+
+    def __getitem__(self, idx):
+        image_path = self._X[idx]
+        image = read_image(image_path)
+        image = self._transform(image)
+        if self._y is not None:
+            label = self._y[idx]
+            return image, label
+        return image
+
+
+class PetfinderDataModule(LightningDataModule):
+    def __init__(
+        self,
+        train_df,
+        val_df,
+        cfg,
+    ):
+        super().__init__()
+        self._train_df = train_df
+        self._val_df = val_df
+        self._cfg = cfg
+
+    def __create_dataset(self, train=True):
+        return (
+            PetfinderDataset(self._train_df, self._cfg.transform.image_size)
+            if train
+            else PetfinderDataset(self._val_df, self._cfg.transform.image_size)
+        )
+
+    def train_dataloader(self):
+        dataset = self.__create_dataset(True)
+        return DataLoader(dataset, **self._cfg.train_loader)
+
+    def val_dataloader(self):
+        dataset = self.__create_dataset(False)
+        return DataLoader(dataset, **self._cfg.val_loader)
+
+
+IMAGENET_MEAN = [0.485, 0.456, 0.406]  # RGB
+IMAGENET_STD = [0.229, 0.224, 0.225]  # RGB
+
+
+def get_default_transforms():
+    transform = {
+        "train": T.Compose(
+            [
+                T.RandomHorizontalFlip(),
+                T.RandomVerticalFlip(),
+                T.RandomAffine(15, translate=(0.1, 0.1), scale=(0.9, 1.1)),
+                T.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
+                T.ConvertImageDtype(torch.float),
+                T.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+            ]
+        ),
+        "val": T.Compose(
+            [
+                T.ConvertImageDtype(torch.float),
+                T.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+            ]
+        ),
+    }
+    return transform
---
+++ train.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import pytorch_lightning as pl
+from pytorch_lightning import callbacks
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger
+from sklearn.model_selection import StratifiedKFold
+
+config = {
+    "seed": 2021,
+    "root": "./",
+    "n_splits": 5,
+    "epoch": 20,
+    "trainer": {
+        "gpus": 1,
+        "accumulate_grad_batches": 1,
+        "progress_bar_refresh_rate": 1,
+        "fast_dev_run": False,
+        "num_sanity_val_steps": 0,
+        "resume_from_checkpoint": None,
+    },
+    "transform": {"name": "get_default_transforms", "image_size": 224},
+    "train_loader": {
+        "batch_size": 64,
+        "shuffle": True,
+        "num_workers": 4,
+        "pin_memory": False,
+        "drop_last": True,
+    },
+    "val_loader": {
+        "batch_size": 64,
+        "shuffle": False,
+        "num_workers": 4,
+        "pin_memory": False,
+        "drop_last": False,
+    },
+    "model": {"name": "swin_tiny_patch4_window7_224", "output_dim": 1},
+    "optimizer": {
+        "name": "optim.AdamW",
+        "params": {"lr": 1e-5},
+    },
+    "scheduler": {
+        "name": "optim.lr_scheduler.CosineAnnealingWarmRestarts",
+        "params": {
+            "T_0": 20,
+            "eta_min": 1e-4,
+        },
+    },
+    "loss": "nn.BCEWithLogitsLoss",
+}
+
+df = pd.read_csv(os.path.join(config.root, "train.csv"))
+df["Id"] = df["Id"].apply(lambda x: os.path.join(config.root, "train", x + ".jpg"))
+
+config = Box(config)
+
+skf = StratifiedKFold(n_splits=config.n_splits, shuffle=True, random_state=config.seed)
+
+for fold, (train_idx, val_idx) in enumerate(skf.split(df["Id"], df["Pawpularity"])):
+    train_df = df.loc[train_idx].reset_index(drop=True)
+    val_df = df.loc[val_idx].reset_index(drop=True)
+    datamodule = PetfinderDataModule(train_df, val_df, config)
+    model = Model(config)
+    earystopping = EarlyStopping(monitor="val_loss")
+    lr_monitor = callbacks.LearningRateMonitor()
+    loss_checkpoint = callbacks.ModelCheckpoint(
+        filename="best_loss",
+        monitor="val_loss",
+        save_top_k=1,
+        mode="min",
+        save_last=False,
+    )
+    logger = TensorBoardLogger(config.model.name)
+
+    trainer = pl.Trainer(
+        logger=logger,
+        max_epochs=config.epoch,
+        callbacks=[lr_monitor, loss_checkpoint, earystopping],
+        **config.trainer,
+    )
+    trainer.fit(model, datamodule=datamodule)
